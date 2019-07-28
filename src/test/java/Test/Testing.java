33a2cbdff86d87a56e4c1aab0cdfbb4597cc8bda
Fathima Sajitha <fathi.saji8@gmail.com>
2019-07-28 10:52:32 +0530

    Unit Testing 1

---
+++ src/test/java/Test/Testing.java
@@ -0,0 +1,32 @@
+package Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Testing {
+	WebDriver driver;
+
+	@Test
+	public void openBrowser() {
+		System.setProperty("webdriver.chrome.driver", "driver//chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	}
+
+	@Test(dependsOnMethods = { "openBrowser" })
+	public void Login() {
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).submit();
+		driver.findElement(By.id("welcome")).click();
+		driver.findElement(By.xpath("//*[@id=\"welcome-menu\"]/ul/li[2]/a")).click();
+	}
+}
